d4ee6aa2788212e959cd6f1e098f4af6d3ed54a7
Adam D. White <adamwhite82@gmail.com>
2017-10-15 16:10:24 -0500

    exercise 5

---
+++ 5_more_variables_and_printing/exercise_5.rb
@@ -0,0 +1,18 @@
+my_name = 'Adam D. White'
+my_age = 35 # not a lie in 2017
+my_height_f = 5 # in feet
+my_height_i = 10 # in inches
+my_weight = 180 # lbs
+my_eyes = 'Hazel'
+my_teeth = 'white'
+my_hair = 'dark brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height_f} feet, #{my_height_i} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "That's a little too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth}, depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height_f}, #{my_height_i}, and #{my_weight} I get #{my_age + my_height_f + my_height_i + my_weight}."
